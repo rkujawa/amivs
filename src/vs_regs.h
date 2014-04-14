c3a96e82ad9888916d105f099a3bdcedfd63c3c9
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2014-04-14 18:18:46 +0200

    Add registers in separate include.

---
+++ src/vs_regs.h
@@ -0,0 +1,16 @@
+#ifndef _VS_REGS_H_
+#define _VS_REGS_H_
+
+#define HW_SPI_DATA_WR_0	0
+#define HW_SPI_DATA_WR_1	1
+#define HW_SPI_DATA_WR_2	2
+#define HW_SPI_DATA_WR_3	3
+#define HW_SPI_DATA_RD_0	4
+#define HW_SPI_DATA_RD_1	5
+#define HW_CPLD_COMMAND		6
+#define HW_CPLD_COMMAND_SEND		0x2	
+#define HW_STATUS		7
+#define HW_SPI_SPEED		8
+#define HW_RESET		9
+
+#endif /* _VS_REGS_H_ */
